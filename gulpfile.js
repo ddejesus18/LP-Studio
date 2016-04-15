e726e210b4f052189b213ae81c8ee346ba83edca
ddejesus18 <careerme1218@gmail.com>
2016-04-15 10:18:40 -0400

    fixed gulpfile.js, everything is working properly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,35 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var compass = require('gulp-compass');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('browser-sync', ['sass'], function() {
+// Start the server
+gulp.task('browser-sync', function() {
     browserSync({
         server: {
-          //changed to local host b/c _site was not being watched and css files not being changed
-            host: "localhost"
+            baseDir: "./"
         }
     });
 });
 
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
- gulp.task('sass', function () {
-   // fixed this broken link to a diff sass folder
-     return gulp.src('_sass/styles.scss')
-         .pipe(sass({
-             includePaths: ['scss'],
-             onError: browserSync.notify
-         }))
-         .pipe(gulp.dest('stylesheets'))
-         .pipe(browserSync.reload({stream:true}))
-         .pipe(gulp.dest('stylesheets'));
- });
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', function () {
-  gulp.watch(['**/*.scss']).on("change", function(file) {
-     browserSync.reload(file.path);
- });
-    gulp.watch(['*.html']);
+// Compile SASS & auto-inject into browsers
+gulp.task('compass', function () {
+    return gulp.src('sass/*.scss')
+        .pipe(compass({
+	      config_file: './config.rb',
+	      css: 'stylesheets',
+	      sass: 'sass'
+	    }))
+        .pipe(reload({stream:true}));
 });
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * launch BrowserSync & watch files.
- */
+// Reload all Browsers
+gulp.task('bs-reload', function () {
+    reload();
+});
 
-
- gulp.task('compass', function () {
-      gulp.src(['sass/*.scss', 'sass/**/*.scss'])
-     .pipe(compass({
-         config_file: './config.rb',
-        css: 'stylesheets',
-         sass: 'sass'
-
-     }))
-     .pipe(gulp.dest("stylesheets"))
- });
-gulp.task('default', ['browser-sync', 'watch','compass']);
+// Watch scss AND html files, doing different things with each.
+gulp.task('default', ['browser-sync', 'compass'], function () {
+    gulp.watch("sass/*.scss", ['compass']);
+    gulp.watch("*.html").on("change", browserSync.reload);
+});
